7cea92462e703c20924e6ad3da78b3f390a61d86
DannyImagineers <danny@theimagineers.com>
2019-10-01 13:00:44 +0200

    Changes to Todo-List

---
+++ src/AddTodoForm.tsx
@@ -0,0 +1,38 @@
+import React, { useState, ChangeEvent, FormEvent } from "react";
+
+interface AddTodoFormProps {
+    addTodo: AddTodo;
+}
+
+export const AddTodoForm: React.FC<AddTodoFormProps> = ({ addTodo }) => {
+    const [newTodo, setNewTodo] = useState("");
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setNewTodo(e.target.value);
+    };
+
+    /* Handle Change */
+    
+    const handleSubmit = (e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        addTodo(newTodo);
+        setNewTodo("");
+    }
+
+    /* Add todo aan list */
+
+    const handleDelete = (e: FormEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        
+    }
+    /* Remove todo van list | Work in progress */
+
+    return (    
+        <form>
+            <input placeholder="Enter your task here" value={newTodo} onChange={handleChange} />
+            <button type="submit" onClick={handleSubmit}>Add Todo</button>
+            <button type="submit" onClick={handleDelete}>Delete Todo</button>
+        </form>
+    );
+};
+    /* Buttons en input */
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { TodoList } from "./TodoList";
+import { AddTodoForm } from './AddTodoForm';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  
+const getExistingTodos = (): Todo[] => {
+    const todoJson = localStorage.getItem("todos");
+    if(!todoJson) return [];
+    const todoArray = JSON.parse(todoJson);
+    return todoArray;
+  };
+  /* LocalStorage getItem */
 
+
+  const [todos, setTodos] = useState(getExistingTodos);
+
+  const toggleTodo: ToggleTodo = selectedTodo => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  /* Toggle Todo's */
+
+  const addTodo: AddTodo = newTodo  => {
+    newTodo.trim() !== "" &&
+    setTodos([...todos, { text: newTodo, complete: false}]);
+    const stringTodos = JSON.stringify([...todos, { text: newTodo, complete: false}]);
+    localStorage.setItem("todos", stringTodos);
+  };
+/* LocalStorage setItem */
+
+  return (
+  <React.Fragment>
+    <TodoList todos={todos} toggleTodo={toggleTodo} />
+    <AddTodoForm addTodo={addTodo} />
+  </React.Fragment>
+  );
+};
 export default App;
